8dd0493e5d03f1749b4a2fef72a3b10c92ee0baa
Timothé Ignace <timothe.ignace@yahoo.fr>
2018-02-26 19:14:58 +0100

    final top-navbar

--- index.php
+++ index.php
@@ -8,28 +8,24 @@
 </head>
 
 <body>
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php"><img src="ressource/logo.png"/>  Bikestore</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav justify-content-end">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php" style="margin-right:20px;">Accueil<span class="sr-only">(current)</span></a>
-      </li>
-      <li>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mr-auto" style="margin-left: 6%;">
       <form class="form-inline form">
-        <input class="form-control mr-sm-2" type="search" placeholder="Tapez votre recherche..." aria-label="Search" style="margin-left: 17px; width: 400px;">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher</button>
+        <input class="form-control mr-sm-2" type="search" placeholder="Tapez votre recherche..." aria-label="Search" style="width: 400px;">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher</button>
       </form>
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <button type="button" class="btn btn-outline-success nav-link transparentbutton" data-toggle="modal" data-target="#login"><img src="ressource/login.png"/>  Connexion</a>
       </li>
       <li class="nav-item">
-        <button type="button" class="btn btn-outline-success nav-link login" data-toggle="modal" data-target="#login"><img src="ressource/login.png"/>  Connexion</a>
+        <button type="button" class="btn btn-outline-success nav-link transparentbutton" data-toggle="modal" data-target="#panier"><img src="ressource/cart.png"/>  Panier</a>
       </li>
-      <li class="nav-item">
-        <button type="button" class="btn btn-outline-success nav-link panier" data-toggle="modal" data-target="#panier"><img src="ressource/cart.png"/>  Panier</a>
-      </li>
+    </ul>
   </div>
 </nav>
 
@@ -71,7 +67,7 @@
                 <label for="exampleInputPassword1">Mot de passe</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
             </div>
-            <button type="submit" class="btn btn-primary">Connecter</button>
+            <button type="submit" class="btn btn-success">Connecter</button>
             </form>
             <a href="form.php" class="form-text text-muted">Pas encore d'identifiant ? Cliquez ici pour vous inscrire.</a>
       </div>
