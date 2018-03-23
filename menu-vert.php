4f00a19c85d7c57db43cbddd08bf847192e1dc7a
Timothé Ignace <timothe.ignace@yahoo.fr>
2018-03-23 15:56:36 +0100

    menu verticale fonctionnel

---
+++ menu-vert.php
@@ -0,0 +1,9 @@
+
+  <div class="container">
+    <div style="margin-top : 10%;">
+      <h5 style="text-align:center;">Qui sommes-nous ?</h5>
+    </div>
+    <div class="fixed-bottom">
+      Administration
+    </div>
+  </div>
